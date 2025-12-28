9b093f7433b111cc2f5ebd4404578032e7e76bd1
KinhLupNe <kinhlupna@gmail.com>
2025-12-29 00:13:38 +0700

    Sufface normal and multi object

--- color.h
+++ color.h
@@ -1,10 +1,8 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "rtweekend.h"
 #include "vec3.h"
-
-#include <iostream>
-
 using color = vec3;
 
 void write_color(std::ostream &out, const color &pixel_color)
--- hittable.h
+++ hittable.h
@@ -1,15 +1,21 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
+#include "rtweekend.h"
 #include "vec3.h"
-
 class hit_record
 {
 public:
   point3 p;
   vec3 normal;
   double t;
+  bool front_face;
+
+  void set_face_normal(const ray &r, const vec3 &outward_normal)
+  {
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
 };
 
 class hittable
