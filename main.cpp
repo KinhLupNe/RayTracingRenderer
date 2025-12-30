dc6ff4e67b59b1bc1a127a9f25c37835d34be0d6
KinhLupNe <kinhlupna@gmail.com>
2025-12-30 23:24:58 +0700

    thay doi mo hinh phan xa anh sang

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ int main()
   camera cam;
   cam.aspect_ratio = 16.0 / 9.0;
   cam.image_width = 400;
-  cam.samples_per_pixel = 100;
+  cam.samples_per_pixel = 200;
+  cam.max_depth = 50;
   cam.render(world);
   return 0;
 }
